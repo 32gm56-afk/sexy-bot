f231a39ae8cea57abc917007dc10cf384ca3a093
32gm56-afk <32gm56@gmail.com>
2026-01-09 01:31:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-from flask import Flask
-import threading
-
 from parser import main_loop
+import threading
+from flask import Flask
 
 app = Flask(__name__)
-
-
-def start_background():
-    t = threading.Thread(target=main_loop, daemon=True)
-    t.start()
-
-
-# стартуємо парсер одразу при запуску сервісу
-start_background()
-
 
 @app.route("/")
 def index():
     return "Sexy-bot is running"
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+# ⬇️ старт парсера
+threading.Thread(target=main_loop, daemon=True).start()
