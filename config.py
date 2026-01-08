4efd8a663cd61962b37ebb5156889d7c7d527047
32gm56-afk <32gm56@gmail.com>
2026-01-09 00:52:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 URL = "https://price.csgetto.love/"
-CHECK_INTERVAL = 35  # seconds
+CHECK_INTERVAL = 50
 
-BOT_TOKEN = os.getenv("8134393467:AAHRcOjVFiy8RTDWSXt3y3u_SDQwYIssK68", "")
-CHAT_ID = os.getenv("-4840038262", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 DATA_FILE = "data.json"
 STATE_FILE = "state.json"
