64a60396e0e5dcd290e8f907f37b66dbe869b72b
32gm56-afk <32gm56@gmail.com>
2026-01-09 04:02:42 +0200

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,14 +1,20 @@
 import requests
 from config import BOT_TOKEN, CHAT_ID
 
+
 def send_telegram(text: str):
+    if not BOT_TOKEN or not CHAT_ID:
+        print("⚠️ Telegram ENV not set", flush=True)
+        return
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
         "text": text,
         "parse_mode": "HTML"
     }
+
     try:
-        requests.post(url, json=payload, timeout=10)
+        requests.post(url, json=payload, timeout=15)
     except Exception as e:
-        print("❌ Telegram error:", e)
+        print("❌ Telegram error:", e, flush=True)
