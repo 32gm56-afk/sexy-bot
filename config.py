3650d27f321d750710aa265bc6069c644e5e0c19
32gm56-afk <32gm56@gmail.com>
2026-01-09 04:02:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+import os
+
 URL = "https://price.csgetto.love/"
 CHECK_INTERVAL = 50  # секунд
 
-BOT_TOKEN = "8134393467:AAHRcOjVFiy8RTDWSXt3y3u_SDQwYIssK68"
-CHAT_ID = "-4840038262"
+# Telegram
+BOT_TOKEN = os.getenv("8134393467:AAHRcOjVFiy8RTDWSXt3y3u_SDQwYIssK68")
+CHAT_ID = os.getenv("-4840038262")
+
+# Proxy (ПІДТЯГУЮТЬСЯ АВТОМАТИЧНО з Render ENV)
+PROXY_HOST = os.getenv("PROXY_HOST")
+PROXY_PORT = os.getenv("PROXY_PORT")
+PROXY_USER = os.getenv("PROXY_USER")
+PROXY_PASS = os.getenv("PROXY_PASS")
