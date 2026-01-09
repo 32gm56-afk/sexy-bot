c2a26beb889a0a2e54ed5dbf284129f52d765f91
32gm56-afk <32gm56@gmail.com>
2026-01-09 03:21:01 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 def round_price(p):
     if p < 0.009:
         return None
-    x = int(round(p * 1000))
-    last = x % 10
-    base = (x // 10) * 10
+
+    p1000 = int(round(p * 1000))
+    last = p1000 % 10
+    base = (p1000 // 10) * 10
+
     if last >= 9:
         base += 10
+
     return base / 1000
 
 
-def format_msg(name, old_p, new_p, qty, kind):
+def format_msg(name, old_p, new_p, qty, typ):
     return (
         f"<code>{name}</code>\n"
-        f"{kind}: {old_p} → {new_p}\n"
+        f"{typ}: {old_p} → {new_p}\n"
         f"Кількість: {qty}"
     )
